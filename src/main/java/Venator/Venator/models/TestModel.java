87b6caebcbf6ae7ea61772ec9848a1eda61cb7d9
chris.wilsonburns <chris.wilsonburns@ruralsourcing.com>
2019-07-11 08:56:45 -0500

    Changed @Configuration in TestModel to @Component

--- src/main/java/Venator/Venator/models/TestModel.java
+++ src/main/java/Venator/Venator/models/TestModel.java
@@ -1,26 +1,34 @@
 package Venator.Venator.models;
 
-import org.springframework.context.annotation.Configuration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class TestModel {
+
+  private static final Log logger = LogFactory.getLog(TestModel.class);
 
   private String character;
   private String system;
 
   public String getCharacter() {
+    logger.info("Get character call: " + character);
     return character;
   }
 
   public void setCharacter(String character) {
     this.character = character;
+    logger.info("Set character call: " + character);
   }
 
   public String getSystem() {
+    logger.info("Get system call: " + system);
     return system;
   }
 
   public void setSystem(String system) {
+    logger.info("Set system call: " + system);
     this.system = system;
   }
 }
