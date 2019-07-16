721b89b145180724a41aab0ce28820a5e18f4b9c
Logan Schanbacher <48256179+rsi-lschanbacher@users.noreply.github.com>
2019-07-16 09:47:09 -0500

    Merge branch 'master' of github.com:Schogan/Venator
    # Conflicts:
    #	src/main/java/Venator/Venator/controller/MainController.java

---
+++ src/main/java/Venator/Venator/service/GetRegion.java
@@ -0,0 +1,28 @@
+package Venator.Venator.service;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+public class GetRegion {
+    public String regionId;
+
+    public String getRegion() throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://esi.evetech.net/latest/universe/regions/"+ regionId +"/?datasource=tranquility&language=en-us")
+                .get()
+                .addHeader("Accept", "*/*")
+                .addHeader("Cache-Control", "no-cache")
+                .addHeader("Host", "esi.evetech.net")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
+    }
+}
