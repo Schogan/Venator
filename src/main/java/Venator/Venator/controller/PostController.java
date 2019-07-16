1c774bb2551f90ba26bbdbdd1b2625f7bad316b6
chris.wilsonburns <chris.wilsonburns@ruralsourcing.com>
2019-07-16 09:27:05 -0500

    Json generation

--- src/main/java/Venator/Venator/controller/PostController.java
+++ src/main/java/Venator/Venator/controller/PostController.java
@@ -2,7 +2,11 @@ package Venator.Venator.controller;
 
 import Venator.Venator.service.GetIdsByName;
 import Venator.Venator.service.GetSystemKills;
+import java.io.FileReader;
 import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +27,30 @@ public class PostController {
   public String getSystemKillsPost() throws IOException {
     String results = getSystemKills.getSystemKills();
     return results;
+  }
+
+  @RequestMapping("/directory")
+  public String directory() throws Exception {
+
+    JSONArray jsonArray =
+        (JSONArray) readJsonSimpleDemo("src/main/resources/json_config/RegionMappings.json");
+    JSONObject obj;
+
+    for (int i = 0; i < jsonArray.size(); i++) {
+      obj = (JSONObject) (jsonArray.get(i));
+      String regionId = obj.get("id").toString();
+
+
+    }
+
+
+
+    return "I guess it worked";
+  }
+
+  public static Object readJsonSimpleDemo(String filename) throws Exception {
+    FileReader reader = new FileReader(filename);
+    JSONParser jsonParser = new JSONParser();
+    return jsonParser.parse(reader);
   }
 }
