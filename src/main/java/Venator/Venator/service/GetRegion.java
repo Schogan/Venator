fdfabe4ff054dd8c468b199e38d5f96d90ec5473
Logan Schanbacher <48256179+rsi-lschanbacher@users.noreply.github.com>
2019-07-16 12:24:28 -0500

    Merge branch 'master' of github.com:Schogan/Venator
    # Conflicts:
    #	src/main/java/Venator/Venator/controller/MainController.java

--- src/main/java/Venator/Venator/service/GetRegion.java
+++ src/main/java/Venator/Venator/service/GetRegion.java
@@ -3,26 +3,29 @@ package Venator.Venator.service;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
-
 import java.io.IOException;
 
 public class GetRegion {
-    public String regionId;
+  public String regionId;
 
-    public String getRegion() throws IOException {
+  public String getRegion() throws IOException {
 
-        OkHttpClient client = new OkHttpClient();
+    OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder()
-                .url("https://esi.evetech.net/latest/universe/regions/"+ regionId +"/?datasource=tranquility&language=en-us")
-                .get()
-                .addHeader("Accept", "*/*")
-                .addHeader("Cache-Control", "no-cache")
-                .addHeader("Host", "esi.evetech.net")
-                .build();
+    Request request =
+        new Request.Builder()
+            .url(
+                "https://esi.evetech.net/latest/universe/regions/"
+                    + regionId
+                    + "/?datasource=tranquility&language=en-us")
+            .get()
+            .addHeader("Accept", "*/*")
+            .addHeader("Cache-Control", "no-cache")
+            .addHeader("Host", "esi.evetech.net")
+            .build();
 
-        Response response = client.newCall(request).execute();
+    Response response = client.newCall(request).execute();
 
-        return response.body().string();
-    }
+    return response.body().string();
+  }
 }
