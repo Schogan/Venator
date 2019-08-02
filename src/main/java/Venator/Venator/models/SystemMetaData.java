06ace9d34fde116d5bb7201b6f7bbbd21f57e6d3
chris.wilsonburns <chris.wilsonburns@ruralsourcing.com>
2019-08-02 09:54:02 -0500

    Added a little pizazz

--- src/main/java/Venator/Venator/models/SystemMetaData.java
+++ src/main/java/Venator/Venator/models/SystemMetaData.java
@@ -1,54 +1,53 @@
 package Venator.Venator.models;
 
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
+import org.springframework.stereotype.Component;
 
 @Component
 public class SystemMetaData {
-    private Long npcKills;
-    private Long podKills;
-    private Long shipKills;
-    private Long jumps;
+  private Long npcKills;
+  private Long podKills;
+  private Long shipKills;
+  private Long jumps;
 
-    public Long getNpcKills() {
-        return npcKills;
-    }
+  public Long getNpcKills() {
+    return npcKills;
+  }
 
-    public void setNpcKills(Long npcKills) {
-        this.npcKills = npcKills;
-    }
+  public void setNpcKills(Long npcKills) {
+    this.npcKills = npcKills;
+  }
 
-    public Long getPodKills() {
-        return podKills;
-    }
+  public Long getPodKills() {
+    return podKills;
+  }
 
-    public void setPodKills(Long podKills) {
-        this.podKills = podKills;
-    }
+  public void setPodKills(Long podKills) {
+    this.podKills = podKills;
+  }
 
-    public Long getShipKills() {
-        return shipKills;
-    }
+  public Long getShipKills() {
+    return shipKills;
+  }
 
-    public void setShipKills(Long shipKills) {
-        this.shipKills = shipKills;
-    }
+  public void setShipKills(Long shipKills) {
+    this.shipKills = shipKills;
+  }
 
-    public Long getJumps() {
-        return jumps;
-    }
+  public Long getJumps() {
+    return jumps;
+  }
 
-    public void setJumps(Long jumps) {
-        this.jumps = jumps;
-    }
+  public void setJumps(Long jumps) {
+    this.jumps = jumps;
+  }
 
-    public ArrayList getSystemMetaData(){
-        ArrayList systemInfo = new ArrayList();
-        systemInfo.add(npcKills);
-        systemInfo.add(podKills);
-        systemInfo.add(shipKills);
-        systemInfo.add(jumps);
-        return systemInfo;
-    }
+  public ArrayList getSystemMetaData() {
+    ArrayList systemInfo = new ArrayList();
+    systemInfo.add(npcKills);
+    systemInfo.add(podKills);
+    systemInfo.add(shipKills);
+    systemInfo.add(jumps);
+    return systemInfo;
+  }
 }
